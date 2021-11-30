c65e215f3847311a2c46e0b559c60595ccc034b3
KrisISCoo1 <88919578+KrisISCoo1@users.noreply.github.com>
2021-11-29 17:14:40 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var input=document.getElementById('input').value;
+function search() {
+    if ((input="dev page") || (input="development website")) {
+        window.location="dev.html";
+        };  
+        else(){
+window.location="web.html";
+        };
+        if ((input="pics") || (input="pictures")) {
+                window.location="pics.html";
+        }
+        if (input="facebook"){
+                window.location="sF.html";
+        }
+        if (input="youtube"){
+                window.location="sF.html";
+        }
+}
